716a105c4852d253ba8025ed2efef226551b72c0
Sakshamebdev873 <vinodarya344@gmail.com>
2025-10-28 13:37:10 +0530

    wrote comments in prisma generate js

--- prisma-generate.js
+++ prisma-generate.js
@@ -6,3 +6,6 @@ dotenv.config();
 console.log("Loaded DATABASE_URL:", process.env.DATABASE_URL ? "✅ Found" : "❌ Missing");
 
 execSync("npx prisma generate", { stdio: "inherit" });
+// execSync comes from Node’s child_process module.
+// It’s used to run another command (like one you’d type in your terminal) from inside your Node script, and “Sync” means it waits for that command to finish before moving on.
+//  stdio: "inherit" it is used to show output in terminal
